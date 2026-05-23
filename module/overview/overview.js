832ba967d6fb33383c03e6a2bd2d01558c5d1353
PhDoom <drjohnpingo@gmail.com>
2026-05-23 18:48:05 -0500

    Update overview.js
    Updated to Foundry 13

--- module/overview/overview.js
+++ module/overview/overview.js
@@ -1,158 +1,63 @@
-const DISPLAY_MODE = {
-  SHOW_ALL: "SHOW_ALL",
-  SHOW_HIDDEN: "SHOW_HIDDEN",
-  SHOW_VISIBLE: "SHOW_VISIBLE",
-};
+/**
+ * Trinity Continuum Actor Overview for Foundry V13.
+ * Provides a summarized view of all player characters.
+ */
+export class TrinityOverview extends Application {
 
-const SIMPLE_SYTEMS = ['swade'];
-
-export class OverviewApp extends Application {
-  constructor(options) {
-    super(options);
-
-    this.hiddenActors = [];
-    this.state = {};
-    this.displayMode = DISPLAY_MODE.SHOW_VISIBLE;
-    this.activeTab = "general";
-
-  }
-
-
-  update() {
-
-
-/** Original
-    console.log("Overview update (before), This:", this);
-    if (typeof this.state.momentum === 'undefined') {
-      console.log("this.state.momentum === 'undefined'");
-      this.state.momentum = {};
-      this.state.momentum.max = game.settings.get("trinity", "momentum-max");
-      this.state.momentum.current = game.settings.get("trinity", "momentum-current");
-      this.state.momentum.spent = game.settings.get("trinity", "momentum-spent");
-    } else {
-      console.log("this.state.momentum !== 'undefined'");
-      if (this.state.momentum.max !== game.settings.get("trinity", "momentum-max")) {
-        console.log("this.state.momentum.max !== game.settings");
-        game.settings.set("trinity", "momentum-max", this.state.momentum.max);
-      }
-      if (this.state.momentum.current !== game.settings.get("trinity", "momentum-current")) {
-        game.settings.set("trinity", "momentum-current", this.state.momentum.current);
-      }
-      if (this.state.momentum.spent !== game.settings.get("trinity", "momentum-spent")) {
-        game.settings.set("trinity", "momentum-spent", this.state.momentum.spent);
-      }
-    }
-    console.log("Overview update (after), This:", this);
-**/
-
-// NEW
-this.state.momentum = {};
-this.state.momentum.max = game.settings.get("trinity", "momentum-max");
-this.state.momentum.current = game.settings.get("trinity", "momentum-current");
-this.state.momentum.spent = game.settings.get("trinity", "momentum-spent");
-
-
-
-    // let actors = game.actors.entities
-    let actors = game.actors.contents
-      .filter(a => a.hasPlayerOwner)
-      .map(playerActor => playerActor.getActiveTokens())
-      .flat(1)
-      .map(token => token.actor);
-
-    // remove duplicates if an actors has multiple tokens on scene
-    actors = actors.reduce(
-      (actors, actor) => (actors.map(a => a.id).includes(actor.id) ? actors : [...actors, actor]),
-      []
-    );
-
-    switch (this.displayMode) {
-      case DISPLAY_MODE.SHOW_HIDDEN:
-        actors = actors.filter(actor => this.hiddenActors.includes(actor.id));
-        break;
-      case DISPLAY_MODE.SHOW_VISIBLE:
-        actors = actors.filter(actor => !this.hiddenActors.includes(actor.id));
-        break;
-    }
-
-    actors = actors.map(actor => {
-      const data = actor.data.data;
-      return this.getActorDetails(actor);
-    });
-
-    this.state = {
-      activeTab: this.activeTab,
-      mode: this.displayMode,
-      actors: actors,
-      momentum: this.state.momentum,
-    };
-
-// Add Icons for Aspirations
-    for (let actor of this.state.actors) {
-      // console.log("Iterating:", actor);
-      actor.aspirations.shortTerm1.icon = "<i class=\"fas fa-stopwatch big flex-valign\"></i>";
-      actor.aspirations.shortTerm2.icon = "<i class=\"fas fa-stopwatch big flex-valign\"></i>";
-      actor.aspirations.longTerm.icon = "<i class=\"fas fa-calendar-alt big flex-valign\"></i>";
-    }
-    // console.log("This, post iteration:", this);
-
-
-  }
-
+  /** @override */
   static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      width: 500,
-      height: 300,
-      resizable: true,
-      title: "Party Overview",
-      template: `systems/trinity/templates/overview/overview.html`,
-      classes: ["overview", game.system.id]
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "trinity-overview",
+      title: "Actor Overview",
+      template: "systems/trinity/templates/overview/overview.html",
+      width: 600,
+      height: "auto",
+      resizable: true
     });
   }
 
-  getData() {
-    this.update();
-    return this.state;
+  /** @override */
+  async getData(options) {
+    // V13: Use game.actors.contents to get the array of actors
+    const actors = game.actors.contents
+      .filter(a => a.type === "character" && (a.testUserPermission(game.user, "OBSERVER") || game.user.isGM))
+      .map(actor => {
+        const sys = actor.system;
+        
+        // Dynamic data extraction based on Trinity subTypes
+        let powerPool = { label: "N/A", value: 0 };
+        if (sys.subType === "talent") powerPool = { label: "Inspiration", value: sys.talent?.inspiration?.value };
+        if (sys.subType === "mage") powerPool = { label: "Quintessence", value: sys.mage?.quintessence?.value };
+        if (sys.subType === "psion") powerPool = { label: "Psi", value: sys.psion?.psi?.value };
+        if (sys.subType === "nova") powerPool = { label: "Quantum", value: sys.nova?.quantum?.value };
+
+        return {
+          id: actor.id,
+          name: actor.name,
+          img: actor.img,
+          subType: sys.subType ? sys.subType.charAt(0).toUpperCase() + sys.subType.slice(1) : "None",
+          health: sys.health,
+          power: powerPool
+        };
+      });
+
+    return {
+      actors: actors
+    };
   }
 
-  htmlDecode(input) {
-    var doc = new DOMParser().parseFromString(input, "text/html");
-    return doc.documentElement.textContent;
-  }
-
-  getActorDetails(actor) {
-    const data = actor.data.data;
-
-      return {
-        id: actor.id,
-        isHidden: this.hiddenActors.includes(actor.id),
-        name: actor.name,
-        aspirations: actor.data.data.aspirations
-      };
-
-  }
-
+  /** @override */
   activateListeners(html) {
     super.activateListeners(html);
 
-    // Change Momentum
-
-    html.find('.momentum').change(ev => {
-      // this.actor.update({ 'data.inspiration.value': ++this.actor.data.data.inspiration.value });
-      // console.log("Momentum Change");
-      // console.log(this.state.momentum.max, this.state.momentum.current, this.state.momentum.spent);
-      // game.settings.set("trinity", "momentum-max", this.state.momentum.max);
-      // game.settings.set("trinity", "momentum-current", this.state.momentum.current);
-      // game.settings.set("trinity", "momentum-spent", this.state.momentum.spent);
-      // console.log(this);
-      console.log(ev);
-      console.log(this.state[ev.currentTarget.name]); // variable
-      console.log(ev.currentTarget.value); // variable
-      console.log(ev.currentTarget.id); // game setting variable
-      game.settings.set("trinity", ev.currentTarget.id, ev.currentTarget.value);
+    // Click to open actor sheet
+    html.find('.actor-name').click(ev => {
+      const actorId = ev.currentTarget.closest('.actor-row').dataset.actorId;
+      const actor = game.actors.get(actorId);
+      actor?.sheet.render(true);
     });
+
+    // GM-only: Refresh the data
+    html.find('.refresh-overview').click(() => this.render(true));
   }
-
 }
-
-// export default OverviewApp;
